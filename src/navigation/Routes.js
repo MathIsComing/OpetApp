8261984b7a122520a68af4f3633babe20143de83
MathIsComing <oguzhan.5055@gmail.com>
2022-01-13 15:35:16 +0300

    standart repo

---
+++ src/navigation/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Routes = () => {
+    return (
+        <View>
+            <Text>Opet app routes</Text>
+        </View>
+    )
+}
+
+export default Routes
